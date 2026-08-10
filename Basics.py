b9b56f965b3fed03e5e66327d97f91b89c235eb0
singhmedha7 <singhmedha2007@gmail.com>
2026-08-10 10:00:00 +0530

    First set of modifications

--- Basics.py
+++ Basics.py
@@ -37,9 +37,9 @@ area=pi*(radius**2)
 print("The area of the circle is ",area)
 
 #Swapping
-'''a=int(input("Enter a: "))
+a=int(input("Enter a: "))
 b=int(input("enter b: "))
 a,b=b,a
-print("After swapping: a=",a," b=",b)'''
+print("After swapping: a=",a," b=",b)
 
 
