eb5c6005c843477d8ec9769ffa10a2235095796d
singhmedha7 <singhmedha2007@gmail.com>
2026-08-10 10:00:00 +0530

    First set of modifications

--- Basics.py
+++ Basics.py
@@ -31,10 +31,10 @@ else:
     print(d," is greatest")
 
 #Area of a circle
-'''radius=float(input("Enter the radius of the circle: "))
+radius=float(input("Enter the radius of the circle: "))
 pi=3.14
 area=pi*(radius**2)
-print("The area of the circle is ",area)'''
+print("The area of the circle is ",area)
 
 #Swapping
 '''a=int(input("Enter a: "))
