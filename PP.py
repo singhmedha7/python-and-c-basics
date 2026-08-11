5e69bca6d1e67590a8ffbe3d9e0c4d4771cacbcf
singhmedha7 <singhmedha2007@gmail.com>
2026-08-11 12:00:00 +0530

    Initial project upload

---
+++ PP.py
@@ -0,0 +1,45 @@
+#Even numbers from 0 to 10
+'''for i in range(0,11):
+    if i%2==0:
+        print(i)'''
+
+#Even or Odd
+'''a=input("Enter a number: ")
+if int(a)%2==0:
+    print("Given number is even")
+else:
+    print("Given number is odd")'''
+
+#Largest among four numbers
+'''List=[]
+for i in range (4):
+    num=int(input("Enter a digit: "))
+    List.append(num)
+print("The largest number is: ",max(List))'''
+
+'''a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+c=int(input("Enter a number: "))
+d=int(input("Enter a number: "))
+if a>b and a>c and a>d:
+    print(a," is greatest")
+elif b>a and b>c and b>d:
+    print(b," is greatest")
+elif c>a and c>b and c>d:
+    print(c," is greatest")
+else:
+    print(d," is greatest")'''
+
+#Area of a circle
+'''radius=float(input("Enter the radius of the circle: "))
+pi=3.14
+area=pi*(radius**2)
+print("The area of the circle is ",area)'''
+
+#Swapping
+'''a=int(input("Enter a: "))
+b=int(input("enter b: "))
+a,b=b,a
+print("After swapping: a=",a," b=",b)'''
+
+
